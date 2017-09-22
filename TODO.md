fa1cf85ce442fc87e4120fe610abe58285f32efb
cwardgar <cwardgar@gmail.com>
2017-09-22 01:02:41 -0600

    Reconfigure Maven repos to mimic old Nexus v2 instance. * Importing repos from v2 showed me that we're stuck with those old names, i.e. "unidata-*". So, embrace that convention and use it for all Maven repo names. * Attempting to build various Unidata projects using v3 showed me that we need analogues for almost all of the old repos from v2. Ugh. So we now have tons of proxy and group repos. * Update Maven tests to only depend on "unit-utils", not (transitively) "udunits". The reason is that we no longer have a proxy repo to the v2 unidata-releases, so "udunits" won't be available in a fresh, test environment. * Removed obsolete test variables that we used before getting the real artifacts cert. * Update to the latest commit of ansible-nexus3-oss, which includes a fix for Maven proxy repos. * Added a TODO item.

--- TODO.md
+++ TODO.md
@@ -4,3 +4,6 @@
 frequently than every two weeks. Monthly maybe?
 1. Add some spiffy Gradle magic in thredds-doc to upload the site to Nexus.
 1. Break up the monolithic Apache config. See the comment at the top of vhosts.conf.
+1. In restore.yml, we rename 'blobs' to 'blobs-old' and then restore 'blobs' from S3. That results in us downloading
+the ENTIRE blobstore from S3. Slow and expensive! Instead, make a copy of 'blobs' at 'blobs-old' and then exploit
+duplicity's rysnc functionality to restore 'blobs' in-place. Should be much faster.
--- provisioning/roles/testing/files/unit-utils/src/main/java/edu/ucar/UnitUtils.java
+++ provisioning/roles/testing/files/unit-utils/src/main/java/edu/ucar/UnitUtils.java
@@ -1,7 +1,5 @@
 package edu.ucar;
 
-import ucar.units.*;
-
 /**
  * Utility class for working with units.
  *
@@ -10,19 +8,13 @@ import ucar.units.*;
  */
 public abstract class UnitUtils {
     /**
-     * Converts {@code quantity} in {@code fromUnit} to the equivalent value in {@code toUnit}.
+     * Converts {@code quantity} in yards to the equivalent value in meters.
      *
      * @param quantity  the value to convert.
-     * @param fromUnit  the unit of {@code quantity}.
-     * @param toUnit    the unit to convert to.
      * @return          the equivalent value.
-     * @throws UnitException  if either of the unit strings is invalid.
      */
-    public static double convert(double quantity, String fromUnit, String toUnit) throws UnitException {
-        Unit inputUnit = UnitFormatManager.instance().parse(fromUnit);
-        Unit outputUnit = UnitFormatManager.instance().parse(toUnit);
-
-        return inputUnit.convertTo(quantity, outputUnit);
+    public static double convertYardsToMeters(double quantity) {
+        return quantity * 0.9144;
     }
 
     // Private constructor to ensure non-instantiability.
