5213f7287f21ad467c2bdb2c6fdc34b5218ae73e
cwardgar <cwardgar@gmail.com>
2017-10-10 22:43:15 -0600

    Restrict IPs in the OpenStack security group, not the VM firewall.
    * As a result, we no longer need 'ansible-role-firewall'. Nuke all traces.
    * Limit SSH access to Unidata network, jenkins-aws, Travis, and cwardgar.
    * Removed a TODO item about using a feature of the Nexus REST API. Turns out that we're already using it!

--- TODO.md
+++ TODO.md
@@ -6,7 +6,4 @@ frequently than every two weeks. Monthly maybe?
 1. In restore.yml, we rename 'blobs' to 'blobs-old' and then restore 'blobs' from S3. That results in us downloading
 the ENTIRE blobstore from S3. Slow and expensive! Instead, make a copy of 'blobs' at 'blobs-old' and then exploit
 duplicity's rysnc functionality to restore 'blobs' in-place. Should be much faster.
-1. As of Nexus 3.3, tasks can be manually run using a simpler REST API endpoint that does *not* involve a Groovy
-script. Let's use that instead in backup.yml. See https://artifacts.unidata.ucar.edu/swagger-ui/#/ and
-http://www.sonatype.org/nexus/2017/09/25/nexus-repository-new-beta-rest-api-for-content/.
 1. On Travis, change the 2nd stage to run site.yml against OpenStack if the 1st stage is successful.
--- orchestration/compute.tf
+++ orchestration/compute.tf
@@ -5,26 +5,89 @@ resource "openstack_compute_keypair_v2" "nexus" {
 
 resource "openstack_compute_secgroup_v2" "nexus" {
   name = "nexus-secgroup"
-  // We'll be running a software firewall on the instance, so go ahead and allow everything at this level.
-  description = "Allow all inbound and outbound IP traffic to/from every address"
+  description = "Allow ICMP, HTTP, and HTTPS from all; allow SSH from select IP addresses."
 
+  // By default, allow all outbound traffic and block all inbound traffic.
+  // Below are rules that selectively permit inbound traffic.
+
+  // Allow all ICMP inbound traffic, for 'ping'. LOOK: Might be able to narrow this.
   rule {
     ip_protocol = "icmp"
     from_port = "-1"
     to_port = "-1"
     cidr = "0.0.0.0/0"
   }
+  // Allow HTTP traffic from all IPs.
   rule {
     ip_protocol = "tcp"
-    from_port = "1"
-    to_port = "65535"
+    from_port = "80"
+    to_port = "80"
     cidr = "0.0.0.0/0"
   }
+  // Allow HTTPS traffic from all IPs.
   rule {
-    ip_protocol = "udp"
-    from_port = "1"
-    to_port = "65535"
+    ip_protocol = "tcp"
+    from_port = "443"
+    to_port = "443"
     cidr = "0.0.0.0/0"
+  }
+
+  // TODO: Do we need to permit NTP traffic on port 123? Test this.
+
+  // Allow SSH traffic from a limited set of IPs.
+  rule {
+    ip_protocol = "tcp"
+    from_port = "22"
+    to_port = "22"
+    cidr = "128.117.140.0/24"  // Unidata
+  }
+  rule {
+    ip_protocol = "tcp"
+    from_port = "22"
+    to_port = "22"
+    cidr = "128.117.144.0/24"  // Unidata
+  }
+  rule {
+    ip_protocol = "tcp"
+    from_port = "22"
+    to_port = "22"
+    cidr = "128.117.156.0/24"  // Unidata
+  }
+  rule {
+    ip_protocol = "tcp"
+    from_port = "22"
+    to_port = "22"
+    cidr = "52.22.28.125/32"  // jenkins-aws
+  }
+  rule {
+    ip_protocol = "tcp"
+    from_port = "22"
+    to_port = "22"
+    cidr = "52.45.185.117/32"  // travis-ci.org. See https://docs.travis-ci.com/user/ip-addresses/
+  }
+  rule {
+    ip_protocol = "tcp"
+    from_port = "22"
+    to_port = "22"
+    cidr = "52.54.31.11/32"  // travis-ci.org
+  }
+  rule {
+    ip_protocol = "tcp"
+    from_port = "22"
+    to_port = "22"
+    cidr = "54.87.185.35/32"  // travis-ci.org
+  }
+  rule {
+    ip_protocol = "tcp"
+    from_port = "22"
+    to_port = "22"
+    cidr = "54.87.141.246/32"  // travis-ci.org
+  }
+  rule {
+    ip_protocol = "tcp"
+    from_port = "22"
+    to_port = "22"
+    cidr = "76.120.68.217/32"  // cwardgar
   }
 }
 
