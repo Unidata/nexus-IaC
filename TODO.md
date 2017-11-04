3cdda6290209a2f9785ccbfe65b3574975b2ec23
cwardgar <cwardgar@gmail.com>
2017-11-03 19:55:05 -0600

    prepare_tools.yml installs all the tools and credentials we'll need * The playbook used to be called prepare_terraform.yml. In addition to Terraform, it now installs Packer and the OpenStack CLI tools. * Rewrote the task that extracts the URLs of the latest versions of Hashicorp tools. It uses jq more extensively and is exhaustively commented. I can now actually follow WTF it's doing! * Renamed destroy_and_recreate_prod.sh to prepare_tools.sh. It now only runs the prepare_tools.yml playbook. Invocation of Terraform is being moved to its own script. * Don't do 'source ~/.openstack/openrc.sh' at the end of prepare_tools.sh; instead we'll do it in each of the scripts that need those credentials. * Removed one TODO item and added another.

--- TODO.md
+++ TODO.md
@@ -10,7 +10,7 @@ than every two weeks. Monthly maybe? A full build and test of THREDDS would exer
 1. In restore.yml, we rename 'blobs' to 'blobs-old' and then restore 'blobs' from S3. That results in us downloading
 the ENTIRE blobstore from S3. Slow and expensive! Instead, make a copy of 'blobs' at 'blobs-old' and then exploit
 duplicity's rysnc functionality to restore 'blobs' in-place. Should be much faster.
-1. On Travis, change the 2nd stage to run site.yml against OpenStack if the 1st stage is successful.
 1. ansible-debian-upgrade-reboot seems to always restart the host, even if nothing was updated. If we're going to run
 site.yml against the OpenStack VM nightly and after every commit, that's gonna lead to unacceptable downtime.
-   - Made a small change to the 'removes' argument of "Reboot host". Maybe that fixed it?
+1. Now that we've incorporated Packer, I don't think we need to provide unidata_provisioner_id_rsa in
+prepare_tools.yml any longer. The key is still needed by backup_prod.sh, however.
--- scripts/prepare_tools.sh
+++ scripts/prepare_tools.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Exit on any individual command failure.
+# Exit immediately if any command exits with a non-zero status.
 set -e
 
 # See https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
@@ -20,19 +20,6 @@ export PYTHONUNBUFFERED=1
 # See http://docs.ansible.com/ansible/latest/playbooks_vault.html#running-a-playbook-with-vault
 export ANSIBLE_VAULT_PASSWORD_FILE="$ansible_dir/files/vault-password"
 
-# Installs Terraform. Decrypts SSH, OpenStack, and AWS credentials and places them in their respective home directories.
-ansible-playbook --verbose prepare_terraform.yml
-
-# Add OpenStack variables to the environment. We created this file in prepare_terraform.yml.
-source ~/.openstack/openrc.sh
-
-cd ../terraform
-
-# See https://www.terraform.io/guides/running-terraform-in-automation.html#auto-approval-of-plans
-
-# One-time Terraform initialization.
-terraform init -input=false
-# Taint the VM, which will cause Terraform to recreate it.
-terraform taint openstack_compute_instance_v2.nexus
-# Apply the changes.
-terraform apply -input=false -auto-approve=true
+# Installs Packer and Terraform.
+# Decrypts SSH, OpenStack, and AWS credentials and places them in their respective home directories.
+ansible-playbook --verbose prepare_tools.yml
